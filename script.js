20da589fde63ea6916f24bcd3ed9065407a39a06
twoton tdr <79171113+twoton-tdr@users.noreply.github.com>
2024-07-27 21:27:13 +0530

    done addition

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-let previousValue=0; //to store the value when adding the next number
-let currentValue=0; //to store the current value and answer
+let previousValue; //to store the value when adding the next number
+let currentValue; //to store the current value and answer
 let previousOperator;
 let currentOperator;
-const equal = document.querySelector("#equals")
-const calculatorScreen = document.querySelector("input");
 
+const calculatorScreen = document.querySelector("input");
 calculatorScreen.addEventListener("keypress",(event)=>{
     const num= '0123456789';
 
@@ -14,29 +13,74 @@ calculatorScreen.addEventListener("keypress",(event)=>{
     }
 })
 
+//setting up numpad
 const numpad = document.querySelectorAll(".num");
-
 for(let i = 0;i<numpad.length;i++){
-    //setting up numpad
     numpad[i].addEventListener("click",(event)=>{
         calculatorScreen.value+=event.target.innerHTML;
     })
 }
 
+//this function is used to prevent entering "." more than once
 const dot = document.querySelector("#dot");
-
 dot.addEventListener("click",(e)=>{
-    //this function is used to prevent entering "." more than once
     if(!calculatorScreen.value.includes(".")){
         calculatorScreen.value+=e.target.innerHTML;
     }
 })
 
-const clearButton = document.querySelector("#clear");
 //adding clear button function
+const clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click",()=>{
     calculatorScreen.value="";
     previousValue = '';
     currentValue = '';
+    previousOperator='';
+    currentOperator='';
 })
+
+//listening to clicks from all operators
+const operators = document.querySelectorAll(".operator");
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click',operation); 
+}
+
+function operation(){
+    if(!calculatorScreen.value){
+        console.log("nothing is in the input");
+        return;
+    }
+
+    currentValue = calculatorScreen.value;
+    calculatorScreen.value='';
+    currentOperator = this.textContent;
+    
+    if(!previousValue){
+        previousValue = currentValue;
+    }
+    else{
+        switch(previousOperator){
+            case '+':addition(previousValue,currentValue)
+            break;
+            case '-': console.log("minus is the prev")
+            break;
+            case 'x': console.log("mult is the prev")
+            break;
+            case '/': console.log("div is the prev")
+            break;
+        }
+        previousValue = '';
+        return;
+    }
+
+
+    previousOperator=currentOperator;
+    currentOperator = "";
+    currentValue = "";
+}
+
+function addition(x,y){
+    calculatorScreen.value =Number(x)+Number(y);
+    return;
+}
 
